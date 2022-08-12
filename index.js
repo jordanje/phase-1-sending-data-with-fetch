dfeb398e0dc699dbf703653771ccfdec3973f498
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-12 11:33:56 -0400

    commit tree ddfd74c3666a849a5d1cdb04f01ba084a978bf21

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail 
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(object => document.body.innerHTML = object["id"])
+    .catch(error => document.body.innerHTML = error.message)
+}
+
+
+
+
+
+  
+
+
